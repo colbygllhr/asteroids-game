7f1d05a0d559b9a69fa2d9facbaff047fceee6bf
colbygllhr <cjg0033@mix.wvu.edu>
2024-08-27 17:28:49 -0400

    added player class, got player object to draw on screen

--- main.py
+++ main.py
@@ -3,27 +3,45 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
+def print_intro():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 def main():
+
+    print_intro()
+    
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+       
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip() #display update
+        seconds = clock.tick(60)
+        dt = seconds / 1000
+        
+
 
     
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
 if __name__ == "__main__":
     main()
